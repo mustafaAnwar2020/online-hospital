b715f9b7443648d250d44620e2f72813c8686123
unknown <sasaxxx777@gmail.com>
2022-10-08 18:36:45 +0200

    routes refactor

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ClinicController;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HospitalController;
+use App\Http\Controllers\ProfessionController;
+use App\Http\Controllers\AppointmentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,42 +21,75 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/profile/edit','App\Http\Controllers\ProfileController@edit')->name('user.edit');
-Route::get('/', 'App\Http\Controllers\HomeController@index');
-Route::get('/home', 'App\Http\Controllers\HomeController@index');
+    Route::get('/profile/edit',[ProfileController::class,'edit'])
+        ->name('user.edit');
+        
+    Route::get('/profile/{User}',[ProfileController::class,'show'])
+        ->name('user.show');
+        
+    Route::resource('/profile',[ProfileController::class]);
 
-Auth::routes();
+    Route::get('/', [HomeController::class,'index']);
+
+    Route::get('/home', [HomeController::class,'index']);
+
+    Auth::routes();
+
+    Route::prefix('admin')->name('admin.')->group(function(){
+
+        Route::resource('/roles',[RoleController::class]);
+        
+        Route::resource('/professions',[ProfessionController::class]);
+    });
+
+    Route::get('/Department/{profession}/Doctors',[DoctorController::class,'professionDoctors'])
+        ->name('dep.doctors');
+
+    Route::get('/doctor/dashboard/Hospital/AddHospital',[DoctorController::class,'createHospital'])
+        ->name('doctor.addHospital');
+
+    Route::post('/doctor/dashboard/Hospital/AddHospital',[DoctorController::class,'storeHospital'])
+        ->name('doctor.storeHospital');
+
+    Route::resource('/doctor/dashboard',[DoctorController::class]);
+
+    Route::get('/doctor/dashboard/Hospital/Show',[HospitalController::class,'showDoctor'])
+        ->name('doctor.showHospital');
+
+    Route::resource('/Hospital',[HospitalController::class]);
+
+    Route::get('/clinic/edit',[ClinicController::class,'edit'])
+        ->name('clinic.edit');
+    
+    Route::get('/clinic/show',[ClinicController::class,'show'])
+        ->name('clinic.show');
+
+    Route::resource('/Clinic',[ClinicController::class]);
 
 
-Route::prefix('admin')->name('admin.')->group(function(){
-    Route::resource('/roles','App\Http\Controllers\RoleController');
-    Route::resource('/professions','App\Http\Controllers\ProfessionController');
+    Route::get('/appointment/doctor/{user}/appointment/{appointment}',[AppointmentController::class,'bookMeeting'])
+        ->name('appointments.bookmeeting');
 
-});
+    Route::post('/appointment/checkout/{app}',[AppointmentController::class,'checkout'])
+        ->name('appointments.checkout');
+    
+    Route::get('/appointment/confirm',[AppointmentController::class,'confirm'])
+        ->name('appointments.confirm');
 
+    Route::get('/appointment/doctor/{user}',[AppointmentController::class,'meeting'])
+        ->name('appointments.meeting');
+    
+    Route::post('/appointment/patient/{appointment}',[AppointmentController::class,'storemeeting'])
+        ->name('appointments.storemeeting');
 
+    Route::get('/appointment/edit',[AppointmentController::class,'edit'])
+        ->name('appointments.edit');
 
-Route::get('/Department/{profession}/Doctors','App\Http\Controllers\DoctorController@professionDoctors')->name('dep.doctors');
-Route::get('/doctor/dashboard/Hospital/Show','App\Http\Controllers\HospitalController@showDoctor')->name('doctor.showHospital');
-Route::get('/doctor/dashboard/Hospital/AddHospital','App\Http\Controllers\DoctorController@createHospital')->name('doctor.addHospital');
-Route::post('/doctor/dashboard/Hospital/AddHospital','App\Http\Controllers\DoctorController@storeHospital')->name('doctor.storeHospital');
-Route::resource('/doctor/dashboard','App\Http\Controllers\DoctorController');
+    Route::get('/appointment/show',[AppointmentController::class,'show'])
+        ->name('appointments.show');
+    
+    Route::put('/appointment/update',[AppointmentController::class,'update'])
+        ->name('appointments.update');
 
-Route::resource('/Hospital','App\Http\Controllers\HospitalController');
-Route::get('/clinic/edit','App\Http\Controllers\ClinicController@edit')->name('clinic.edit');
-Route::get('/clinic/show','App\Http\Controllers\ClinicController@show')->name('clinic.show');
-Route::resource('/Clinic','App\Http\Controllers\ClinicController');
-Route::get('/appointment/doctor/{user}/appointment/{appointment}','App\Http\Controllers\AppointmentController@bookMeeting')->name('appointments.bookmeeting');
-Route::post('/appointment/checkout/{app}','App\Http\Controllers\AppointmentController@checkout')->name('appointments.checkout');
-Route::get('/appointment/confirm','App\Http\Controllers\AppointmentController@confirm')->name('appointments.confirm');
-Route::get('/appointment/doctor/{user}','App\Http\Controllers\AppointmentController@meeting')->name('appointments.meeting');
-Route::post('/appointment/patient/{appointment}','App\Http\Controllers\AppointmentController@storemeeting')->name('appointments.storemeeting');
-Route::get('/appointment/edit','App\Http\Controllers\AppointmentController@edit')->name('appointments.edit');
-Route::get('/appointment/show','App\Http\Controllers\AppointmentController@show')->name('appointments.show');
-Route::put('/appointment/update','App\Http\Controllers\AppointmentController@update')->name('appointments.update');
-Route::resource('/appointment','App\Http\Controllers\AppointmentController');
-
-Route::get('/profile/{User}','App\Http\Controllers\ProfileController@show')->name('user.show');
-
-Route::resource('/profile','App\Http\Controllers\ProfileController');
+    Route::resource('/appointment',[AppointmentController::class]);
 
